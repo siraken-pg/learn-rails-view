d1acfe875f21bf4fc3d92be8d3f186f362c8c8f3
Kento Shirasawa <shirasawa.kento@gmail.com>
2023-11-11 00:38:54 +0900

    feat: rspec

--- spec/helpers/posts_helper_spec.rb
+++ spec/helpers/posts_helper_spec.rb
@@ -1,15 +1,10 @@
 require 'rails_helper'
 
-# Specs in this file have access to a helper object that includes
-# the PostsHelper. For example:
-#
-# describe PostsHelper do
-#   describe "string concat" do
-#     it "concats two strings with spaces" do
-#       expect(helper.concat_strings("this","that")).to eq("this that")
-#     end
-#   end
-# end
-RSpec.describe PostsHelper, type: :helper do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe PostsHelper do
+  # pending "add some examples to (or delete) #{__FILE__}"
+  describe "string concat" do
+    it "one plus one is two" do
+      expect(1 + 1).to eq(2)
+    end
+  end
 end
--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -1,7 +1,11 @@
 require 'rails_helper'
 
-RSpec.describe "Posts", type: :request do
+describe "Posts", type: :request do
   describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+    # pending "add some examples (or delete) #{__FILE__}"
+    it "returns http 404" do
+      get "/posts/index"
+      expect(response).to have_http_status(404)
+    end
   end
 end
