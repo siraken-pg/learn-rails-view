715d28f1cad06091704816f0c2086f1192f4ceaa
Kento Shirasawa <shirasawa.kento@gmail.com>
2023-11-11 01:25:52 +0900

    feat: Create, Read

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,31 @@
 class PostsController < ApplicationController
   def index
-    @posts = [{title: 'John'}, {title: 'Jane'}]
+    @posts = Post.all
     render 'posts/index'
+  end
+
+  def show
+    @post = Post.find(params[:id])
+    render 'posts/details'
+  end
+
+  def new
+    @post = Post.new
+
+    render 'posts/form'
+  end
+
+  def create
+    # @post = Post.new(post_params)
+    @post = Post.new(title: params[:title], body: params[:body])
+    if @post.save
+      redirect_to posts_path, notice: 'Post was successfully created.'
+    else
+      render 'posts/form'
+    end
+  end
+
+  def post_params
+    params.require(:post).permit(:title, :body)
   end
 end
